8f74b66fe0c4a78bda2d1489d53a17f7bf38dd46
tasneem821 <11410120220089@stud.cu.edu.eg>
2024-10-25 21:43:03 +0300

    Update Task2.cpp add iterative and recursive binary search

--- Task2.cpp
+++ Task2.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include "vector"
+#include "algorithm"
 using namespace std;
 
 int RecursiveSearch(int arr[] , int n , int value , int i=0)
@@ -10,43 +12,43 @@ int RecursiveSearch(int arr[] , int n , int value , int i=0)
     return RecursiveSearch(arr , n , value , i+1);
 }
 // find number in sorted array using binary search but iterative
- int iterative_binary_search(vector<int>&numbers,int target){
-     int least_number=0;
-     int highest_number=numbers.size()-1;
-     while (least_number<=highest_number){
-         int mid_number= least_number+(highest_number-least_number)/2;
-         if(numbers[mid_number]==target){
-             return mid_number;
-         }
-         else if(numbers[mid_number]<target){
-             least_number=mid_number+1;
-         }
-         else{
-             highest_number = mid_number-1;
-         }
-     }
-     return -1;
- }
- // recursive binary search that return the target number from sorted array
- int recursive_binary_search(vector<int>&numbers,int target,int least_number,int highest_number){
+int iterative_binary_search(vector<int>&numbers,int target){
+    int least_number=0;
+    int highest_number=numbers.size()-1;
+    while (least_number<=highest_number){
+        int mid_number= least_number+(highest_number-least_number)/2;
+        if(numbers[mid_number]==target){
+            return mid_number;
+        }
+        else if(numbers[mid_number]<target){
+            least_number=mid_number+1;
+        }
+        else{
+            highest_number = mid_number-1;
+        }
+    }
+    return -1;
+}
+// recursive binary search that return the target number from sorted array
+int recursive_binary_search(vector<int>&numbers,int target,int least_number,int highest_number){
 
-     if (least_number<=highest_number){
-         int mid_number= least_number+(highest_number-least_number)/2;
-         if(numbers[mid_number]==target){
-             //base case
-             return mid_number;
-         }
-         else if(numbers[mid_number]<target){
-             // see numbers that highest than middle number
-             return recursive_binary_search(numbers,target,mid_number+1,highest_number);
-         }
-         else{
-             //see the numbers that is lower than the middle number
-             return recursive_binary_search(numbers,target,least_number,mid_number-1);
-         }
-     }
-     return -1;
- }
+    if (least_number<=highest_number){
+        int mid_number= least_number+(highest_number-least_number)/2;
+        if(numbers[mid_number]==target){
+            //base case
+            return mid_number;
+        }
+        else if(numbers[mid_number]<target){
+            // see numbers that highest than middle number
+            return recursive_binary_search(numbers,target,mid_number+1,highest_number);
+        }
+        else{
+            //see the numbers that is lower than the middle number
+            return recursive_binary_search(numbers,target,least_number,mid_number-1);
+        }
+    }
+    return -1;
+}
 void displayMenu() {
     cout << "Menu:\n";
     cout << "1. Iterative Sequential Search\n";
@@ -90,51 +92,51 @@ int main()
         }
         else if(c==3)
         {
-               vector<int>nums;
-     cout<<"please enter the size of array\n";
-     int n;
-     cin>>n;
-     cout<<"please enter the numbers in array \n";
-     for(int i=0;i<n;i++){
-         int x;
-         cin>>x;
-         nums.push_back(x);
-     }
-     sort(nums.begin(),nums.end());
-   cout<<"pls enter the target number\n";
-             int target;
-             cin>>target;
-             int res = iterative_binary_search(nums,target);
-             if(res<0){
-                 cout<<"the target number is not found\n";
-             }
-             else{
-                 cout<<"the number is found in index "<<res<<" and its "<<nums[res]<<"\n";
-             }
+            vector<int>nums;
+            cout<<"please enter the size of array\n";
+            int n;
+            cin>>n;
+            cout<<"please enter the numbers in array \n";
+            for(int i=0;i<n;i++){
+                int x;
+                cin>>x;
+                nums.push_back(x);
+            }
+            sort(nums.begin(),nums.end());
+            cout<<"pls enter the target number\n";
+            int target;
+            cin>>target;
+            int res = iterative_binary_search(nums,target);
+            if(res<0){
+                cout<<"the target number is not found\n";
+            }
+            else{
+                cout<<"the number is found in index "<<res<<" and its "<<nums[res]<<"\n";
+            }
         }
         else if(c==4)
         {
-               vector<int>nums;
-     cout<<"please enter the size of array\n";
-     int n;
-     cin>>n;
-     cout<<"please enter the numbers in array \n";
-     for(int i=0;i<n;i++){
-         int x;
-         cin>>x;
-         nums.push_back(x);
-     }
-     sort(nums.begin(),nums.end());
-      cout<<"pls enter the target number\n";
-             int target;
-             cin>>target;
-             int res = recursive_binary_search(nums,target,0,nums.size()-1);
-             if(res<0){
-                 cout<<"the target number is not found\n";
-             }
-             else{
-                 cout<<"the number is found in index "<<res<<" and its "<<nums[res]<<"\n";
-             }
+            vector<int>nums;
+            cout<<"please enter the size of array\n";
+            int n;
+            cin>>n;
+            cout<<"please enter the numbers in array \n";
+            for(int i=0;i<n;i++){
+                int x;
+                cin>>x;
+                nums.push_back(x);
+            }
+            sort(nums.begin(),nums.end());
+            cout<<"pls enter the target number\n";
+            int target;
+            cin>>target;
+            int res = recursive_binary_search(nums,target,0,nums.size()-1);
+            if(res<0){
+                cout<<"the target number is not found\n";
+            }
+            else{
+                cout<<"the number is found in index "<<res<<" and its "<<nums[res]<<"\n";
+            }
         }
         else
         {
@@ -145,5 +147,3 @@ int main()
 
 
 }
-
-
